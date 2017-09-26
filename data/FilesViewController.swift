6519f01fd28531042264236b72b974a77da0c592
Arno Solin <arno.solin@gmail.com>
2017-09-26 09:39:42 +0300

    Initial commit. Working version of IMU data collection. Ugly UI.

---
+++ data/FilesViewController.swift
@@ -0,0 +1,106 @@
+//
+//  FilesViewController.swift
+//  data
+//
+//  Created by Arno Solin on 25.9.2017.
+//  Copyright © 2017 Arno Solin. All rights reserved.
+//
+
+import UIKit
+
+class FilesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    /* Outlets */
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Add table view
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func listFilesFromDocumentsFolder() -> [String]? {
+        let fileMngr = FileManager.default;
+        
+        // Full path to documents directory
+        let docs = fileMngr.urls(for: .documentDirectory, in: .userDomainMask)[0].path
+        
+        // List all contents of directory and return as [String] OR nil if failed
+        return try? fileMngr.contentsOfDirectory(atPath:docs)
+    }
+    
+    //MARK: - Tableview Delegate & Datasource
+    func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
+        let list = listFilesFromDocumentsFolder()
+        return (list?.count)!
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        let list = listFilesFromDocumentsFolder()
+        cell.textLabel?.text = list?[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        // All files
+        let list = listFilesFromDocumentsFolder()
+        
+        // Path to file
+        let documentsPath = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        let filePath = NSURL(fileURLWithPath: documentsPath.absoluteString).appendingPathComponent((list?[indexPath.row])!)
+        
+        // Present share dialog
+        let activityViewController = UIActivityViewController(activityItems: [filePath!], applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        self.present(activityViewController, animated: true, completion: nil)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            // All files
+            let list = listFilesFromDocumentsFolder()
+            
+            // Path to file
+            let documentsPath = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            let filePath = NSURL(fileURLWithPath: documentsPath.absoluteString).appendingPathComponent((list?[indexPath.row])!)
+
+            // File Manager
+            let fileManager = FileManager.default;
+            
+            // Delete file
+            do {
+                try fileManager.removeItem(at: filePath!);
+            } catch let error as NSError {
+                print("Error removing file:\n \(error)")
+            }
+            
+            // Delete the table view row
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        } else if editingStyle == .insert {
+            // Not used, but if you were adding a new row, this is where you would do it.
+        }
+    }
+    
+}
---
+++ data/ViewController.swift
@@ -0,0 +1,247 @@
+//
+//  ViewController.swift
+//  data
+//
+//  Created by Arno Solin on 23.9.2017.
+//  Copyright © 2017 Arno Solin. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class ViewController: UIViewController {
+    
+    /* Constants */
+    let ACCELEROMETER_ID = 3
+    let GYROSCOPE_ID     = 4
+    let MAGNETOMETER_ID  = 5
+    let BAROMETER_ID     = 6
+    let GRAVITY          = 9.81
+    let ACCELEROMETER_DT = 0.01
+    let GYROSCOPE_DT     = 0.01
+    let MAGNETOMETER_DT  = 0.01
+
+    /* Outlets */
+    @IBOutlet weak var toggleButton: UIButton!
+    
+    /* Managers for the sensor data */
+    let motionManager = CMMotionManager()
+    let altimeter = CMAltimeter()
+    
+    /* Variables */
+    var isCapturing : Bool = false
+    var outputStream : OutputStream!
+    var filename : String = ""
+    var filePath : NSURL!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Tap gesture for start/stop
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.toggleCapture(_:)))
+        tap.numberOfTapsRequired = 1
+        toggleButton.addGestureRecognizer(tap);
+        
+    }
+
+    override func viewDidLayoutSubviews() {
+        
+        toggleButton.frame = CGRect(x: (self.view.frame.size.width - 80) / 2, y: (self.view.frame.size.height - 100), width: 80, height: 80)
+        toggleButton.layer.borderWidth = 6
+        toggleButton.layer.cornerRadius = toggleButton.frame.height/2.0
+        toggleButton.layer.masksToBounds = true
+        toggleButton.layer.borderColor = UIColor.red.cgColor
+        toggleButton.layer.backgroundColor = UIColor.white.cgColor
+        toggleButton.layer.shadowColor = UIColor.white.cgColor
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func toggleCapture(_ sender: UITapGestureRecognizer) {
+        
+        if (!isCapturing) {
+            
+            print("Attempting to start capture");
+            
+            /* Create filename for the data */
+            let date = Date()
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd-HH-mm-ss"
+            filename = "data-" + formatter.string(from: date)
+            print(filename)
+            
+            /* Create output stream */
+            filePath = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(filename)!.appendingPathExtension("csv") as NSURL
+            outputStream = OutputStream(url: filePath as URL, append: false)
+            if outputStream != nil {
+                outputStream.open()
+            } else {
+                print("Unable to open file.")
+                return
+            }
+            
+            /* Start accelerometer updates */
+            if motionManager.isAccelerometerAvailable && !motionManager.isAccelerometerActive {
+                motionManager.accelerometerUpdateInterval = self.ACCELEROMETER_DT
+                motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: {(accelerometerData: CMAccelerometerData!, error: Error!) in
+                    if (error != nil){
+                        print("\(error)");
+                    }
+                    let str = NSString(format:"%f,%d,%f,%f,%f\n",
+                        accelerometerData.timestamp,
+                        self.ACCELEROMETER_ID,
+                        accelerometerData.acceleration.x * self.GRAVITY,
+                        accelerometerData.acceleration.y * self.GRAVITY,
+                        accelerometerData.acceleration.z * self.GRAVITY)
+                    if self.outputStream.write(str as String) < 0 { print("Write failure"); }
+                    } as CMAccelerometerHandler)
+            } else {
+                print("No accelerometer available.");
+            }
+            
+            /* Start gyroscope updates */
+            if motionManager.isGyroAvailable && !motionManager.isGyroActive {
+                motionManager.gyroUpdateInterval = self.GYROSCOPE_DT
+                motionManager.startGyroUpdates(to: OperationQueue.current!, withHandler: {(gyroData: CMGyroData!, error: Error!) in
+                    let str = NSString(format:"%f,%d,%f,%f,%f\n",
+                        gyroData.timestamp,
+                        self.GYROSCOPE_ID,
+                        gyroData.rotationRate.x,
+                        gyroData.rotationRate.y,
+                        gyroData.rotationRate.z)
+                    if self.outputStream.write(str as String) < 0 { print("Write failure"); }
+                } as CMGyroHandler)
+            } else {
+                print("No gyroscope available.");
+            }
+            
+            /* Start magnetometer updates */
+            if motionManager.isMagnetometerAvailable && !motionManager.isMagnetometerActive {
+                motionManager.magnetometerUpdateInterval = self.MAGNETOMETER_DT
+                motionManager.startMagnetometerUpdates(to: OperationQueue.current!, withHandler: {(magnetometerData: CMMagnetometerData!, error: Error!) in
+                    if (error != nil){
+                        print("\(error)");
+                    }
+                    let str = NSString(format:"%f,%d,%f,%f,%f\n",
+                        magnetometerData.timestamp,
+                        self.MAGNETOMETER_ID,
+                        magnetometerData.magneticField.x,
+                        magnetometerData.magneticField.y,
+                        magnetometerData.magneticField.z)
+                    if self.outputStream.write(str as String) < 0 { print("Write failure"); }
+                } as CMMagnetometerHandler)
+            } else {
+                print("No magnetometer available.");
+            }
+            
+            /* Start barometer updates */
+            if CMAltimeter.isRelativeAltitudeAvailable() {
+                altimeter.startRelativeAltitudeUpdates(to: OperationQueue.current!, withHandler: {(altitudeData: CMAltitudeData!, error: Error!)in
+                    if (error != nil){
+                        print("\(error)");
+                    }
+                    let str = NSString(format:"%f,%d,%f,%f,0\n",
+                        altitudeData.timestamp,
+                        self.BAROMETER_ID,
+                        altitudeData.pressure.doubleValue,
+                        altitudeData.relativeAltitude.doubleValue)
+                    if self.outputStream.write(str as String) < 0 { print("Write failure"); }
+                } as CMAltitudeHandler)
+            } else {
+                print("No barometer available.");
+            }
+            
+            /* Start platform location updates */
+            // TODO
+            
+            /* Start capturing */
+            isCapturing = true;
+            self.toggleButton.setTitle("Stop", for: .normal);
+            animateButtonRadius(toValue: toggleButton.frame.height/10.0)
+            
+        } else {
+            
+            print("Attempting to stop capture");
+            
+            /* Stop capturing */
+            isCapturing = false
+            self.toggleButton.setTitle("Start", for: .normal)
+            animateButtonRadius(toValue: toggleButton.frame.height/2.0)
+            
+            /* Stop capture */
+            motionManager.stopAccelerometerUpdates();
+            motionManager.stopGyroUpdates();
+            motionManager.stopMagnetometerUpdates();
+            altimeter.stopRelativeAltitudeUpdates();
+            
+            /* Close output stream */
+            outputStream.close()
+            
+            /* Move data file */
+            let documentsPath = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            let destinationPath = NSURL(fileURLWithPath: documentsPath.absoluteString).appendingPathComponent(filename)?.appendingPathExtension("csv")
+            let fileManager = FileManager.default
+            do {
+                try fileManager.moveItem(at: filePath as URL, to: destinationPath!)
+            } catch let error as NSError {
+                print("Error occurred while moving data file:\n \(error)")
+            }
+            
+        }
+        
+    }
+    
+    @IBAction func unwindToMain(segue: UIStoryboardSegue) {
+    }
+    
+    func animateButtonRadius(toValue: CGFloat) {
+        let animation = CABasicAnimation(keyPath:"cornerRadius")
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.fromValue = toggleButton.layer.cornerRadius
+        animation.toValue = toValue
+        animation.duration = 1.0
+        toggleButton.layer.add(animation, forKey: "cornerRadius")
+        toggleButton.layer.cornerRadius = toValue
+    }
+    
+}
+
+// Extension to OutputStream: Write Strings
+extension OutputStream {
+    
+    func write(_ string: String, encoding: String.Encoding = .utf8, allowLossyConversion: Bool = false) -> Int {
+        
+        if let data = string.data(using: encoding, allowLossyConversion: allowLossyConversion) {
+            return data.withUnsafeBytes { (bytes: UnsafePointer<UInt8>) -> Int in
+                var pointer = bytes
+                var bytesRemaining = data.count
+                var totalBytesWritten = 0
+                
+                while bytesRemaining > 0 {
+                    let bytesWritten = self.write(pointer, maxLength: bytesRemaining)
+                    if bytesWritten < 0 {
+                        return -1
+                    }
+                    
+                    bytesRemaining -= bytesWritten
+                    pointer += bytesWritten
+                    totalBytesWritten += bytesWritten
+                }
+                
+                return totalBytesWritten
+            }
+        }
+        
+        return -1
+    }
+    
+}
+
+
+
+
+
