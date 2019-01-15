cc78382eef597d2f1f8651a6bf380a924f31fe1a
Pekka Rantalankila <pekka.rantalankila@aalto.fi>
2019-01-15 12:11:15 +0200

    Connect UI buttons to camera mode enum.

--- data/SettingsViewController.swift
+++ data/SettingsViewController.swift
@@ -4,6 +4,14 @@ protocol SettingsTableViewDelegate {
     func settingsTableViewCell(cell: SettingsTableViewCell, newSwitchValue: Bool, cellTag : String)
 }
 
+// Ordered list of segmented control options.
+private let cameraModeOptions = [
+    ("AV Camera", CameraMode.AV),
+    ("ARKit", CameraMode.ARKit),
+]
+
+private let cameraModeKey = "cameraMode"
+
 class SettingsViewController: UIViewController {
     @IBOutlet weak private var settingsTable: UITableView!
     @IBOutlet weak var cameraModeControl: UISegmentedControl!
@@ -13,6 +21,17 @@ class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Construct the segmented control manually so that we know which
+        // button index corresponds to which mode. So that we can't for example
+        // accidentally change the order of buttons in the storyboard and
+        // break the code.
+        cameraModeControl.removeAllSegments()
+        for (i, mode) in cameraModeOptions.enumerated() {
+            cameraModeControl.insertSegment(withTitle: mode.0, at: i, animated: true)
+        }
+        // Returns 0 is key not set.
+        cameraModeControl.selectedSegmentIndex = UserDefaults.standard.integer(forKey: cameraModeKey)
 
         settingsTable.dataSource = self
         settingsTable.separatorStyle = UITableViewCell.SeparatorStyle.none
@@ -33,11 +52,16 @@ class SettingsViewController: UIViewController {
         }
     }
 
+    private func getCameraMode() -> CameraMode {
+        return cameraModeOptions[cameraModeControl.selectedSegmentIndex].1
+    }
+
     @IBAction func cameraModeControlValueChanged(_ sender: UISegmentedControl) {
+        UserDefaults.standard.set(cameraModeControl.selectedSegmentIndex, forKey: cameraModeKey)
         // Change camera mode on change of the setting (or exit from the settings view) so that the
         // camera preview view updates and that the start capture button won't stall to setup camera.
         // TODO
-        // captureControllerDelegate.startCamera()
+        // captureControllerDelegate.startCamera(getCameraMode())
     }
 }
 
