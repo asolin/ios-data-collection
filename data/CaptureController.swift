5c9db7976cebea14c8194f23f6272d6a1f7e71cd
Pekka Rantalankila <pekka.rantalankila@aalto.fi>
2019-10-16 17:22:20 +0300

    Enable ARKit auto focus setting (the default).

--- data/CaptureController.swift
+++ data/CaptureController.swift
@@ -67,7 +67,7 @@ class CaptureController: NSObject {
         // Resolution and autofocus can be set starting from iOS 11.3.
         // The default is best resolution and auto focus enabled.
         if #available(iOS 11.3, *) {
-            arConfiguration.isAutoFocusEnabled = false
+            // arConfiguration.isAutoFocusEnabled = false
             // for f in ARWorldTrackingConfiguration.supportedVideoFormats {
             //     if f.imageResolution.height == 1080 {
             //         // With default resolution 1920x1440, the 1920x1080 resulting video with this setting seems to be stretched.
